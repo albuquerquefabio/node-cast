d6a9a11e392feb45b530536a4d7060c6e8633597
Fábio Albuquerque <albuquerquefabio@icloud.com>
2024-08-28 22:48:08 -0300

    chore: Remove commented out code in auth.service.ts

--- apps/node-cast/src/auth/auth.service.ts
+++ apps/node-cast/src/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
-// import { User } from '../users/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { CacheService } from '../cache/cache.service';
 import { ttl } from '../cache/cache-constants';
