b3c8613ec1c37741d6ad53322407afdd9c8ab3ab
Fábio Albuquerque <6733883+albuquerquefabio@users.noreply.github.com>
2024-08-29 18:41:28 -0300

    Fix: database configuration and providers for PostgreSQL (#7)
    * fix: database configuration and providers for PostgreSQL
    
    * chore: Update PostgreSQL host configuration in CI unit test workflow
    
    * chore: Increase health retries in CI unit test workflow and remove commented code in database module spec

--- apps/node-cast/src/shared/database.module.spec.ts
+++ apps/node-cast/src/shared/database.module.spec.ts
@@ -1,14 +1,32 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { DatabaseModule } from './database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('DatabaseModule', () => {
   let module: TestingModule;
 
   beforeAll(async () => {
     module = await Test.createTestingModule({
-      imports: [DatabaseModule],
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          // envFilePath: '.env',
+        }),
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: process.env.POSTGRES_HOST || 'localhost',
+          port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+          username: process.env.POSTGRES_USER || 'postgres',
+          password: process.env.POSTGRES_PASSWORD || 'postgres123',
+          database: process.env.POSTGRES_DB || 'node_cast',
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: true,
+        }),
+        DatabaseModule,
+      ],
     }).compile();
-  });
+  }, 10000);
 
   it('should be defined', () => {
     expect(module).toBeDefined();
--- apps/node-cast/src/shared/database.providers.ts
+++ apps/node-cast/src/shared/database.providers.ts
@@ -8,11 +8,12 @@ export const DatabaseProviders = [
     useFactory: async (configService: ConfigService) => {
       return {
         type: 'postgres',
-        host: configService.get<string>('POSTGRES_HOST'),
-        port: parseInt(configService.get<string>('POSTGRES_PORT')),
-        username: configService.get<string>('POSTGRES_USER'),
-        password: configService.get<string>('POSTGRES_PASSWORD'),
-        database: configService.get<string>('POSTGRES_DB'),
+        host: configService.get<string>('POSTGRES_HOST') ?? '127.0.0.1',
+        port: parseInt(configService.get<string>('POSTGRES_PORT') ?? '5432'),
+        username: configService.get<string>('POSTGRES_USER') ?? 'postgres',
+        password:
+          configService.get<string>('POSTGRES_PASSWORD') ?? 'postgres123',
+        database: configService.get<string>('POSTGRES_DB') ?? 'node_cast',
         autoLoadEntities: true,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
