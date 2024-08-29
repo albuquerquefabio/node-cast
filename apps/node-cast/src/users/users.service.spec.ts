85a181f150b4963ae844f9fc36e1558dca67fc47
Fábio Albuquerque <albuquerquefabio@icloud.com>
2024-08-28 22:00:14 -0300

    feat: Add UpdateUserDto for updating user information

--- apps/node-cast/src/users/users.service.spec.ts
+++ apps/node-cast/src/users/users.service.spec.ts
@@ -5,10 +5,19 @@ import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserRoles } from './interfaces/users.interface';
+import { UsersRepository } from './repositories/users.repository';
+import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
+jest.mock('bcrypt', () => ({
+  compare: jest.fn(),
+  hash: jest.fn(),
+}));
 describe('UsersService', () => {
   let service: UsersService;
   let repository: Repository<User>;
+  let usersRepo: UsersRepository;
+  let user: User;
   const id = Date.now().toString();
 
   beforeEach(async () => {
@@ -19,11 +28,31 @@ describe('UsersService', () => {
           provide: getRepositoryToken(User),
           useClass: Repository,
         },
+        {
+          provide: UsersRepository,
+          useValue: {
+            findByEmail: jest.fn(),
+            findByUsername: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
     repository = module.get<Repository<User>>(getRepositoryToken(User));
+    usersRepo = module.get<UsersRepository>(UsersRepository);
+
+    user = {
+      id,
+      name: 'Jack Daniel',
+      username: 'jackdaniel' + id,
+      email: `jack.daniel.${id}@example.com`,
+      password: 'hashedPassword',
+      status: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      roles: [UserRoles.USER],
+    };
   });
 
   it('should be defined', () => {
@@ -47,8 +76,11 @@ describe('UsersService', () => {
 
       jest.spyOn(repository, 'create').mockReturnValue(user);
       jest.spyOn(repository, 'save').mockResolvedValue(user);
+      (bcrypt.hash as jest.Mock).mockReturnValue('hashedPassword');
 
-      expect(await service.create(createUserDto)).toEqual(user);
+      const result = await service.create(createUserDto);
+
+      expect(result).toEqual(user);
     });
 
     it('should throw an error if required fields are missing', async () => {
@@ -67,19 +99,7 @@ describe('UsersService', () => {
 
   describe('findAll', () => {
     it('should return an array of users', async () => {
-      const users: User[] = [
-        {
-          id,
-          name: 'Jack Daniel',
-          username: 'jackdaniel' + id,
-          email: `jack.daniel.${id}@example.com`,
-          password: 'password123',
-          status: true,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          roles: [UserRoles.USER],
-        },
-      ];
+      const users: User[] = [user];
 
       jest.spyOn(repository, 'find').mockResolvedValue(users);
 
@@ -89,29 +109,115 @@ describe('UsersService', () => {
 
   describe('findOne', () => {
     it('should return a user by id', async () => {
-      const user: User = {
-        id,
-        name: 'Jack Daniel',
-        username: 'jackdaniel' + id,
-        email: `jack.daniel.${id}@example.com`,
-        password: 'password123',
-        status: true,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        roles: [UserRoles.USER],
-      };
-
-      jest.spyOn(repository, 'findOneOrFail').mockResolvedValue(user);
+      jest.spyOn(repository, 'findOne').mockResolvedValue(user);
 
       expect(await service.findOne(id)).toEqual(user);
     });
 
     it('should throw an error if user is not found', async () => {
       jest
-        .spyOn(repository, 'findOneOrFail')
+        .spyOn(repository, 'findOne')
         .mockRejectedValue(new Error('User not found'));
 
-      await expect(service.findOne(id)).rejects.toThrow('User not found');
+      expect(service.findOne(id)).rejects.toThrow('User not found');
     });
   });
+
+  describe('update', () => {
+    it('should update the password', async () => {
+      const userData: UpdateUserDto = {
+        password: 'newPassword',
+        oldPassword: 'password',
+      };
+
+      jest.spyOn(repository, 'findOne').mockResolvedValue(user);
+      jest
+        .spyOn(repository, 'save')
+        .mockResolvedValue({ ...user, password: 'newHashedPassword' });
+      (bcrypt.hash as jest.Mock).mockResolvedValue('newHashedPassword');
+      (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+
+      const { password } = await service.findOne(user.id);
+
+      const result = await service.update(user.id, {
+        password: userData.password,
+        oldPassword: userData.oldPassword,
+      });
+
+      expect(bcrypt.compare).toHaveBeenCalledWith(
+        userData.oldPassword,
+        password
+      );
+      expect(bcrypt.hash).toHaveBeenCalledWith(userData.password, 10);
+      expect(repository.save).toHaveBeenCalledWith({
+        ...user,
+        password: 'newHashedPassword',
+      });
+      delete user['oldPassword'];
+      expect(result).toEqual({ ...user, password: 'newHashedPassword' });
+    });
+    it('should update the username', async () => {
+      const userData: UpdateUserDto = {
+        username: 'newUsername',
+      };
+
+      jest.spyOn(repository, 'findOne').mockResolvedValue(user);
+      jest.spyOn(repository, 'save').mockResolvedValue({
+        ...user,
+        username: userData.username,
+      });
+
+      const result = await service.update(user.id, userData);
+
+      expect(result.username).toBe(userData.username);
+      expect(repository.save).toHaveBeenCalledWith({
+        ...user,
+        username: userData.username,
+      });
+    });
+    it('should throw an error if user sends a duplicated username', async () => {
+      const userData: UpdateUserDto = {
+        username: 'existingUsername',
+      };
+
+      jest.spyOn(repository, 'findOne').mockResolvedValueOnce(user);
+      jest.spyOn(usersRepo, 'findByUsername').mockResolvedValueOnce({
+        ...user,
+        username: userData.username,
+      });
+      jest.spyOn(repository, 'save').mockImplementation();
+
+      await expect(service.update(user.id, userData)).rejects.toThrow(
+        'Username already in use.'
+      );
+      expect(usersRepo.findByUsername).toHaveBeenCalledWith(userData.username);
+      expect(repository.save).not.toHaveBeenCalled();
+    });
+    it('should throw an error if user sends the wrong password confirmation', async () => {
+      const userData: UpdateUserDto = {
+        password: 'newPassword',
+        oldPassword: 'wrongOldPassword',
+      };
+
+      jest.spyOn(repository, 'findOne').mockResolvedValue(user);
+      (bcrypt.compare as jest.Mock).mockResolvedValue(false);
+      jest.spyOn(repository, 'save').mockImplementation();
+
+      const { password } = await service.findOne(user.id);
+
+      await expect(
+        service.update(user.id, {
+          password: userData.password,
+          oldPassword: userData.oldPassword,
+        })
+      ).rejects.toThrow('Password incorrect.');
+
+      expect(bcrypt.compare).toHaveBeenCalledWith(
+        userData.oldPassword,
+        password
+      );
+      expect(repository.save).not.toHaveBeenCalled();
+    });
+  });
+  // describe('delete', () => {});
 });
--- apps/node-cast/src/users/users.service.ts
+++ apps/node-cast/src/users/users.service.ts
@@ -1,14 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersRepository } from './repositories/users.repository';
+import { checkPassword, hashPassword } from '../util/utility';
+import { IUsersService } from './interfaces/users.interface';
 
 @Injectable()
-export class UsersService {
+export class UsersService implements IUsersService {
   constructor(
     @InjectRepository(User)
-    private usersRepository: Repository<User>
+    private usersRepository: Repository<User>,
+    private usersRepo: UsersRepository
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
@@ -18,21 +27,96 @@ export class UsersService {
       throw new Error('Missing required fields');
     }
 
+    await this.hasUser({ username, email });
+
+    const hashPassword = await this.hashPassword(password);
+
     const user = this.usersRepository.create({
       name,
       email,
       username,
-      password,
+      password: hashPassword,
     });
 
     return await this.usersRepository.save(user);
   }
 
   async findAll(): Promise<Array<User>> {
-    return this.usersRepository.find();
+    return this.usersRepository.find({
+      where: { status: true },
+      select: { password: false },
+    });
   }
 
-  async findOne(id: string): Promise<User> {
-    return this.usersRepository.findOneOrFail({ where: { id } });
+  async findOne(
+    id: string,
+    select?: Partial<Record<keyof User, boolean>>
+  ): Promise<Partial<User>> {
+    return this.usersRepository.findOne({
+      where: [
+        ...(!isNaN(+id) ? [{ id, status: true }] : []),
+        { username: id, status: true },
+        { email: id, status: true },
+      ],
+      ...(select && Object.keys(select).length ? select : {}),
+    });
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: { id } });
+
+    if (!user) throw new NotFoundException('User not found.');
+
+    if (updateUserDto.username)
+      await this.hasUser({ username: updateUserDto.username });
+
+    if (updateUserDto.password && updateUserDto.oldPassword) {
+      await this.checkPassword(updateUserDto.oldPassword, user.password);
+      updateUserDto.password = await this.hashPassword(updateUserDto.password);
+    }
+
+    Object.assign(user, updateUserDto);
+
+    return await this.usersRepository.save(user);
+  }
+
+  async delete(id: string): Promise<void> {
+    const user = await this.usersRepository.findOne({
+      where: { id },
+      select: { id: true },
+    });
+    if (!user) throw new NotFoundException('User not found');
+
+    await this.usersRepository.update(id, { status: false });
+  }
+
+  private async hasUser(
+    identifier: Partial<Pick<CreateUserDto, 'username' | 'email'>>
+  ): Promise<void> {
+    if (identifier.email) {
+      const existingUserByEmail = await this.usersRepo.findByEmail(
+        identifier.email
+      );
+      if (existingUserByEmail) {
+        throw new ConflictException('Email already in use.');
+      }
+    }
+
+    if (identifier.username) {
+      const existingUserByUsername = await this.usersRepo.findByUsername(
+        identifier.username
+      );
+      if (existingUserByUsername) {
+        throw new ConflictException('Username already in use.');
+      }
+    }
+  }
+
+  private async hashPassword(password: string): Promise<string> {
+    return await hashPassword(password);
+  }
+
+  private async checkPassword(password: string, hashed: string): Promise<void> {
+    await checkPassword(password, hashed);
   }
 }
