c40be8cecd55636869b074b10cff014f3310d579
Fábio Albuquerque <albuquerquefabio@icloud.com>
2024-08-31 11:46:08 -0300

    chore: Update app.tsx to include UserProvider for managing user data and authentication

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -5,45 +5,44 @@ import Navbar from './components/navbar/Navbar';
 import { ApiProvider } from './context/ApiContext';
 import Panel from './components/panel/Panel';
 import PrivateRouter from './components/PrivateRoute';
+import { UserProvider } from './context/UserContext';
 
 export function App() {
   return (
     <ApiProvider>
-      <div>
-        {/* START: routes */}
-        {/* These routes and navigation have been generated for you */}
-        {/* Feel free to move and update them to fit your needs */}
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                This is the generated root route.{' '}
-                <Link to="/page-2">Click here for page 2.</Link>
-              </div>
-            }
-          />
-          <Route
-            path="/page-2"
-            element={
-              <div>
-                <Link to="/">Click here to go back to root page.</Link>
-              </div>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/panel"
-            element={
-              <PrivateRouter>
-                <Panel />
-              </PrivateRouter>
-            }
-          />
-        </Routes>
-        {/* END: routes */}
-      </div>
+      <UserProvider>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <div>
+                  This is the generated root route.{' '}
+                  <Link to="/page-2">Click here for page 2.</Link>
+                </div>
+              }
+            />
+            <Route
+              path="/page-2"
+              element={
+                <div>
+                  <Link to="/">Click here to go back to root page.</Link>
+                </div>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/panel"
+              element={
+                <PrivateRouter>
+                  <Panel />
+                </PrivateRouter>
+              }
+            />
+          </Routes>
+        </div>
+      </UserProvider>
     </ApiProvider>
   );
 }
