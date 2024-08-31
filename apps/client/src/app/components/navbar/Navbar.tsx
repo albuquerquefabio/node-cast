91cae7a2718e026be590a6014f30d3f3c648182b
Fábio Albuquerque <albuquerquefabio@icloud.com>
2024-08-31 11:45:19 -0300

    feat: Add logout functionality to Navbar component

--- apps/client/src/app/components/navbar/Navbar.tsx
+++ apps/client/src/app/components/navbar/Navbar.tsx
@@ -1,35 +1,38 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import Brand from '../brand/Brand';
+import { useUser } from '../../context/UserContext';
 
 export const Navbar = () => {
   const [nav, setNav] = useState(false);
+  const { user, setUser } = useUser();
+  const navigate = useNavigate();
 
   const handleNav = () => {
     setNav(!nav);
   };
 
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    if (nav) handleNav();
+    setUser(null);
+  };
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
+
   const navItems = [
     { id: 'home', text: 'Home', to: '/' },
-    { id: 'login', text: 'Login', to: '/login' },
+    ...(user ? [] : [{ id: 'login', text: 'Login', to: '/login' }]),
+    ...(user ? [{ id: 'logout', text: 'Logout', onClick: handleLogout }] : []),
   ];
 
   return (
     <nav className="relative bg-gray-800 z-10">
-      {/* <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Login">Login</Link>
-          </li>
-        </ul>
-      </div> */}
       <div className="flex justify-between items-center h-24 max-w-[1240px] max-auto px-4 text-white z-10">
         <Brand />
 
@@ -39,7 +42,11 @@ export const Navbar = () => {
               key={item.id}
               className="p-4 hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-gray-800"
             >
-              <Link to={item.to} className="block w-full h-full">
+              <Link
+                to={item.to || '#'}
+                onClick={item.onClick}
+                className="block w-full h-full"
+              >
                 {item.text}
               </Link>
             </li>
@@ -57,13 +64,19 @@ export const Navbar = () => {
               : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
           }
         >
+          {user?.username && <li className="p-4">{user.username}</li>}
+
           {navItems.map((item) => (
             <li
               key={item.id}
               className="border-b hover:bg-[#00df9a] duration-300 hover:text-gray-800 cursor-pointer border-gray-600"
               onClick={handleNav}
             >
-              <Link to={item.to} className="p-4 block w-full h-full">
+              <Link
+                to={item.to || '#'}
+                onClick={item.onClick}
+                className="p-4 block w-full h-full"
+              >
                 {item.text}
               </Link>
             </li>
