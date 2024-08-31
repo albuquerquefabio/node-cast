f7bbcd50be99298f28bd8d43d454c55d34c999b0
Fábio Albuquerque <albuquerquefabio@icloud.com>
2024-08-31 11:45:09 -0300

    feat: Add getUserData method to ApiService for fetching user data from the API

--- apps/client/src/app/interfaces/IApiService.ts
+++ apps/client/src/app/interfaces/IApiService.ts
@@ -6,4 +6,5 @@ export interface ApiProviderProps {
 
 export interface IApiService {
   login(username: string, password: string): Promise<{ access_token: string }>;
+  getUserData<T>(): Promise<T>;
 }
